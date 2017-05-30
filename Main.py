1c0865618131ab2eb020d7ad3b59138c517be033
GaryPetroFFF <petruchcho2003@gmail.com>
2017-05-30 21:46:45 +0300

    H(s), h(t), h1(t)

--- Main.py
+++ Main.py
@@ -6,13 +6,25 @@
 import Classes
 import CreateChain
 import MeshMethod
+import LaplaceTransformDomain as LTD
+import sympy
+import numpy
 
-chain = CreateChain.default_chain()
+# Модуль 1: t-область
+# chain = CreateChain.default_chain()
 # chain = CreateChain.create_chain()
-MeshMethod.mesh_method(chain)
-chain.output_chain()
+# MeshMethod.mesh_method(chain)
+# chain.output_chain()
+# Модуль 2: S-область
+HS_num, HS_den, H1S_den, H_num, H_den = LTD.HS_calc([[-2, -1], [1, 0]], [[1], [0]], [[1, 2]], 1)
+print('H(S) =', sympy.sympify('('+HS_num+')/('+HS_den+')'))
+print('H1(S) =', sympy.sympify('('+HS_num+')/('+H1S_den+')'))
+ht = LTD.S2t(sympy.sympify('('+HS_num+')/('+HS_den+')'))
+print('h(t) =', ht)
+h1t = LTD.S2t(sympy.sympify('('+HS_num+')/('+H1S_den+')'))
+print('h1(t) =', h1t)
+FR, PR = LTD.FRandPR(H_num, H_den)
+print('АЧХ =', FR)
+print('ФЧХ =', PR)
 
-__all__ = ['Classes', Classes.Element, Classes.I, Classes.V, Classes.R, Classes.L, Classes.C, Classes.Chain,
-           Classes.Node]
-
-__all__ = ['CreateChain', 'MeshMethod']
+__all__ = ['Classes', 'sympy', 'CreateChain', 'MeshMethod']
